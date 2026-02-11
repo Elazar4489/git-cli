ab65d336f45857cdcfa9bd2d778f0b47a7ce4a8e
Elazar Altman <emma298450@gmail.com>
2026-02-11 14:18:19 +0200

    Requirement 2

--- level1.py
+++ level1.py
@@ -1,5 +1,10 @@
 
-def getting_the_path_to_the_log_file(path):
+def generates_a_matrix_of_data(path):
     with open(path, 'r', encoding="utf-8") as log:
         list_of_lines_lists=[w for w in [l.split(",") for l in log.readlines()]]
     return list_of_lines_lists
+
+def extracting_external_IP_addresses(data):
+    list_of=[i[1] for i in data if not i[1].startswith("192.168") and not i[1].startswith("10.")]
+    return list_of
+
