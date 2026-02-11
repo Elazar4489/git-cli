b6657e95aaacc53206035afc477f4fbb16acc63c
Elazar Altman <emma298450@gmail.com>
2026-02-11 15:03:05 +0200

    Requirement 4

--- level1.py
+++ level1.py
@@ -4,6 +4,8 @@ def generates_a_matrix_of_data(path):
         matrix_of_data=[w for w in [l.split(",") for l in log.readlines()]]
     return matrix_of_data
 
+a=generates_a_matrix_of_data('network_traffic.log')
+
 def extracting_external_IP_addresses(data):
     list_of_external_IP=[i[1] for i in data if not i[1].startswith("192.168") and not i[1].startswith("10.")]
     return list_of_external_IP
@@ -11,3 +13,8 @@ def extracting_external_IP_addresses(data):
 def filtering_by_sensitive_port(data):
     list_of_sensitive_port = [i for i in data if  i[3]== '22' or i[3]== '23' or i[3]== '3389']
     return list_of_sensitive_port
+
+def filter_by_size(data):
+    list_of_packages_larger_than_5000=[i for i in data if int(i[-1]) > 5000]
+    return list_of_packages_larger_than_5000
+
