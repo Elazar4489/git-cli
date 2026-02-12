5110e6aea502cca4c64be73cc0debf8b6648df2c
Elazar Altman <emma298450@gmail.com>
2026-02-12 16:52:08 +0200

    Requirement 5(3)

--- level3.py
+++ level3.py
@@ -9,3 +9,9 @@ package_size_conversion= list(map(lambda b: int(b[-1])/1024,g('network_traffic.l
 filter_rows_by_port=list(filter(lambda p:p[-3] == "22" or p[-3]== "23" or p[-3]=="3389",g('network_traffic.log')))
 
 nighttime_activity_filtering=list(filter(lambda n: "00:00:00" < n[0][11:19] < "06:00:00", g('network_traffic.log')))
+
+suspicion_checks = { "EXTERNAL_IP": lambda row: True if not row[1].startswith("192.168") and not row[1].startswith("10.") else False,
+                     "SENSITIVE_PORT": lambda row: True if  row[3]== '22' or row[3]== '23' or row[3]== '3389' else False,
+                     "LARGE_PACKET":lambda row: True if int(row[-1]) > 5000 else False,
+                     "NIGHT_ACTIVITY": lambda row: True if "00:00:00" < row[0][11:19] < "06:00:00" else False}
+
