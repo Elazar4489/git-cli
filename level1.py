9988db71195cdebc35ee61e1dd01b32436fd4076
Elazar Altman <emma298450@gmail.com>
2026-02-12 13:12:37 +0200

    Requirement 3(2)

--- level1.py
+++ level1.py
@@ -4,11 +4,11 @@ def generates_a_matrix_of_data(path):
         matrix_of_data=[w for w in [l.split(",") for l in log.readlines()]]
     return matrix_of_data
 
-def extracting_external_IP_addresses(data):
+def extracting_external_IP_addresses(data) -> list[str]:
     list_of_external_IP=[i[1] for i in data if not i[1].startswith("192.168") and not i[1].startswith("10.")]
     return list_of_external_IP
 
-def filtering_by_sensitive_port(data):
+def filtering_by_sensitive_port(data) -> list[list[str]]:
     list_of_sensitive_port = [i for i in data if  i[3]== '22' or i[3]== '23' or i[3]== '3389']
     return list_of_sensitive_port
 
--- level2.py
+++ level2.py
@@ -1,5 +1,5 @@
-from level1 import generates_a_matrix_of_data
-a=generates_a_matrix_of_data('network_traffic.log')
+import level1 as l1
+a=l1.generates_a_matrix_of_data('network_traffic.log')
 def counting_requests_by_IP(data):
     ips = [row[1] for row in data]
     return {ip: ips.count(ip) for ip in ips}
@@ -16,3 +16,23 @@ def counting_requests_by_IP(data):
 def port_to_protocol_mapping(data):
     dict_of_mapped_port={row[3]: row[4] for row in data}
     return dict_of_mapped_port
+
+def filter_by_time(data):
+    return [row for row in data if "00:00:00" < row[0][11:19] < "06:00:00"]
+
+def suspicious_detection_for_each_IP(data):
+    listush=[row[1] for row in data]
+
+    aa={"EXTERNAL_IP": l1.extracting_external_IP_addresses(data),
+        "SENSITIVE_PORT": list(map(lambda ip: ip[1],l1.filtering_by_sensitive_port(data))),
+        "LARGE_PACKET": list(map(lambda ip: ip[1],l1.filter_by_size(data))),
+        "NIGHT_ACTIVITY": list(map(lambda ip: ip[1],filter_by_time(data)))}
+    g={}
+    for ip in listush:
+        for key,value in aa.items():
+            if ip in value and ip not in g:
+                g[ip]={key}
+            elif ip in value:
+                g[ip].add(key)
+    yy= {k: list(v) for k,v in g.items()}
+    return yy
