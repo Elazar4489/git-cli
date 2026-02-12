0cb7138df87a9f871d0fd4e0045cca83c23d3417
Elazar Altman <emma298450@gmail.com>
2026-02-12 15:44:53 +0200

    Requirement 1,(3)

--- level3.py
+++ level3.py
@@ -2,5 +2,4 @@ import level1 as l1
 
 a=l1.generates_a_matrix_of_data('network_traffic.log')
 def extracting_time_from_timestamp(data):
-    return list(map(lambda s: s[0][11:13],data))
-
+    return list(map(lambda s: int(s[0][11:13]),data))
