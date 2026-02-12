1aa29a0f6601142b71eace9c69a6fd174016010d
Elazar Altman <emma298450@gmail.com>
2026-02-12 16:19:08 +0200

    Requirement 4(3)

--- level3.py
+++ level3.py
@@ -7,3 +7,5 @@ extracting_time_from_timestamp=list(map(lambda s: int(s[0][11:13]),g('network_tr
 package_size_conversion= list(map(lambda b: int(b[-1])/1024,g('network_traffic.log')))
 
 filter_rows_by_port=list(filter(lambda p:p[-3] == "22" or p[-3]== "23" or p[-3]=="3389",g('network_traffic.log')))
+
+nighttime_activity_filtering=list(filter(lambda n: "00:00:00" < n[0][11:19] < "06:00:00", g('network_traffic.log')))
