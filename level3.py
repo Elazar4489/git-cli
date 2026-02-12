09ce801cfefde5cf1f82107134155932297909a8
Elazar Altman <emma298450@gmail.com>
2026-02-12 16:00:30 +0200

    Requirement 2(3)

--- level3.py
+++ level3.py
@@ -3,3 +3,6 @@ import level1 as l1
 a=l1.generates_a_matrix_of_data('network_traffic.log')
 def extracting_time_from_timestamp(data):
     return list(map(lambda s: int(s[0][11:13]),data))
+
+def package_size_conversion(data):
+    return list(map(lambda b: int(b[-1])/1024,data))
