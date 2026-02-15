25c0503a9431a2447bec12248e506bf82a25dcce
Elazar Altman <emma298450@gmail.com>
2026-02-15 10:41:38 +0200

    Requirement 4(4)

--- level4.py
+++ level4.py
@@ -14,3 +14,6 @@ def returning_suspicions_with_row_details(path):
     k = [(line,running_tests_on_a_line(line))  for line in generates_a_matrix_of_data(path) if len(running_tests_on_a_line(line)) >= 1]
     return k
 
+def counting_without_loading_into_memory(path):
+    counter=sum(1 for line in generates_a_matrix_of_data(path) if len(running_tests_on_a_line(line)) >=1)
+    return counter
