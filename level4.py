1d17a9ce4965d999bb79c4c84bee5edaac8a0694
Elazar Altman <emma298450@gmail.com>
2026-02-15 10:21:00 +0200

    Requirement 2(4)

--- level4.py
+++ level4.py
@@ -1,4 +1,4 @@
-
+from level3 import *
 
 def generates_a_matrix_of_data(path):
     with open(path, 'r', encoding="utf-8") as log:
@@ -6,6 +6,7 @@ def generates_a_matrix_of_data(path):
             line=[w for w in line.split(",")]
             yield list(line)
 
-
-a=generates_a_matrix_of_data('network_traffic.log')
+def filtering_suspicious_lines_with_yield(path):
+    k=[line for line in generates_a_matrix_of_data(path) if len(running_tests_on_a_line(line)) >=1]
+    return k
 
