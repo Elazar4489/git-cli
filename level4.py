fc46ce44b063f4911f61f4a0dec703d2e2dcffa9
Elazar Altman <emma298450@gmail.com>
2026-02-15 10:29:13 +0200

    Requirement 3(4)

--- level4.py
+++ level4.py
@@ -10,3 +10,7 @@ def filtering_suspicious_lines_with_yield(path):
     k=[line for line in generates_a_matrix_of_data(path) if len(running_tests_on_a_line(line)) >=1]
     return k
 
+def returning_suspicions_with_row_details(path):
+    k = [(line,running_tests_on_a_line(line))  for line in generates_a_matrix_of_data(path) if len(running_tests_on_a_line(line)) >= 1]
+    return k
+
